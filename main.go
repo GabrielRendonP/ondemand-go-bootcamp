f8d925d788bde3805f73e24d9627ab1280ec3893
Gabriel Rendon Paredes <gabriel.rendon@wizeline.com>
2022-05-10 12:24:50 -0500

    Add services and controller instances to main file

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import (
 	"net/http"
 
 	"github.com/GabrielRendonP/ondemand-go-bootcamp/controllers"
+	"github.com/GabrielRendonP/ondemand-go-bootcamp/repo"
+	"github.com/GabrielRendonP/ondemand-go-bootcamp/services"
 )
 
 func main() {
+	lr := repo.NewLocalData()
+	ns := services.NewService(lr)
+	nc := controllers.NewController(ns)
+
 	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/pokemons", controllers.GetPokemons)
-	http.HandleFunc("/pokemon", controllers.GetPokemon)
+	http.HandleFunc("/pokemons", nc.GetPokemons)
+	http.HandleFunc("/pokemon", nc.GetPokemon)
 
 	_ = http.ListenAndServe(":8080", nil)
 }
