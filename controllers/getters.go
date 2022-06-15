b3eab33c22d6c825b420fc8a5132f817be69d46d
Gabriel Rendon Paredes <gabriel.rendon@wizeline.com>
2022-06-15 12:56:08 -0500

    Add controller function for concurrent read

--- controllers/getters.go
+++ controllers/getters.go
@@ -2,10 +2,15 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/GabrielRendonP/ondemand-go-bootcamp/helpers"
 )
 
+// GetPokemon finds and returns a pokemon by id
 func (c controller) GetPokemon(w http.ResponseWriter, r *http.Request) {
 
 	query := r.URL.Query()
@@ -22,6 +27,7 @@ func (c controller) GetPokemon(w http.ResponseWriter, r *http.Request) {
 	w.Write(response)
 }
 
+// GetPokemons returns all pokemons from csv file
 func (c controller) GetPokemons(w http.ResponseWriter, r *http.Request) {
 
 	pokeList, err := c.s.GetAllPokemons()
@@ -37,6 +43,7 @@ func (c controller) GetPokemons(w http.ResponseWriter, r *http.Request) {
 	w.Write(response)
 }
 
+// PokemonIndex shows all pokemons found on external poke api
 func (c controller) PokemonIndex(w http.ResponseWriter, r *http.Request) {
 	list, _ := c.s.GetAllPokemonsFromApi()
 
@@ -45,6 +52,7 @@ func (c controller) PokemonIndex(w http.ResponseWriter, r *http.Request) {
 	w.Write(response)
 }
 
+// SavePokeApi Stores a pokemon list from external poke api
 func (c controller) SavePokeApi(w http.ResponseWriter, r *http.Request) {
 	list, _ := c.s.GetAllPokemonsFromApi()
 	err := c.s.SaveToCsv(list)
@@ -57,4 +65,33 @@ func (c controller) SavePokeApi(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"message": "Data Saved to CSV"}`))
+}
+
+// ConcurrentRead displays read results from csv or errors
+func (c controller) ConcurrentRead(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	var err error
+	err = helpers.ValidateParams(query)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		msg := `{%s: %s}`
+		w.Write([]byte(fmt.Sprintf(msg, "error", err.Error())))
+		return
+	}
+	ipw, _ := strconv.Atoi(query.Get("ipw"))
+	items, _ := strconv.Atoi(query.Get("items"))
+	types := query.Get("type")
+
+	list, err := c.s.ConcurrentRead(ipw, items, types)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		msg := `{%s: %s}`
+		w.Write([]byte(fmt.Sprintf(msg, "error", err.Error())))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	response, _ := json.Marshal(list)
+	w.Write(response)
+
 }
